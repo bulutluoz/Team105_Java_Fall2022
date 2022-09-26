c61bccc0c96b5009bc60473a770475a063d2e754
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-09-26 10:09:44 +0200

    ilk ders

---
+++ src/ders01_programlamayaGiris/C01_HelloWorld.java
@@ -0,0 +1,22 @@
+package ders01_programlamayaGiris;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+
+        // IT dunyasina merhaba diyelim
+
+        System.out.println("Hello World");
+
+        /*
+        Birden fazla
+        satiri yorum haline
+        getirmek icin
+        bu yontem kullanilir
+         */
+
+    }
+
+
+}
+
---
+++ src/ders01_programlamayaGiris/C02_Variables.java
@@ -0,0 +1,43 @@
+package ders01_programlamayaGiris;
+
+public class C02_Variables {
+
+    public static void main(String[] args) {
+
+
+        int sayi = 111;
+
+        System.out.println("Hello Java");
+
+
+        System.out.println(sayi); // 111
+
+        /*
+          java'da iki turlu yazdirma yapabiliriz
+          eger cift tirnak icindde bir bilgi yazdirirsak
+          java be yazmissak aynen konsolda yazdirir
+
+          Ancak " " olmayan bir sey gorurse
+          onun yazdirilacak bir metin degil
+          bir deger tasiyan variable oldugunu anlar
+          hafizada o variable icin ayirdigi bolume gider
+          o bolumde hangi deger varsa onu yazdirir
+         */
+
+        int not;
+
+        not=50;
+
+        System.out.println(not); // 50
+
+
+
+        not= 70;
+
+
+        System.out.println(not); //70
+
+    }
+
+
+}
