e6c691c3ff94d442213f8e63b5ff3d311f2442a0
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-10-01 23:57:19 +0200

    7.gun sonu

---
+++ src/ders07_NestedIfElseStatements/C04_NestedIfElseStatements.java
@@ -0,0 +1,52 @@
+package ders07_NestedIfElseStatements;
+
+import java.util.Scanner;
+
+public class C04_NestedIfElseStatements {
+    public static void main(String[] args) {
+        // Soru 1- Kullanicidan cinsiyetini ve yasini alin, Kadin, 60 yas ve uzeri ,
+        // Erkek 65 yas ve uzeri emekli olabilir.
+        // Cinsiyet ve yasini dikkate alarak
+        // “Emekli olabilirsin” veya “Emekli olmak icin .. Yil daha calisman gerekir” yazdirin.
+
+        // ana degisken yas olsun
+
+
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Lutfen yasinizi giriniz");
+        double yas= scan.nextDouble();
+
+        System.out.println("Lutfen cinsiyeti giriniz E/K");
+        char cinsiyet= scan.next().charAt(0);
+
+        if (yas<15){
+            System.out.println("Hatali yas girisi");
+        } else if (yas<60) {
+            // emekli olamaz, calismasi gereken sure cinsiyete bagli
+            System.out.println("emekli olamazsin");
+            if (cinsiyet=='K' || cinsiyet=='k'){
+                System.out.println("daha "+ (60-yas)+ " sene calismalisin");
+            } else if (cinsiyet=='E' || cinsiyet=='e') {
+                System.out.println("daha "+ (65-yas)+ " sene calismalisin");
+            }else{
+                System.out.println("hatali cinsiyet girisi");
+            }
+
+        } else if (yas<65) {
+            // kadin ise emekli olabilir, erkek calismasi lazim
+            if (cinsiyet=='K' || cinsiyet=='k'){
+                System.out.println("emekli olabilirsin");
+            } else if (cinsiyet=='E'|| cinsiyet=='e') {
+                System.out.println("emekli olamk icin " + (65-yas)+ " yil daha calismalisin");
+            }else {
+                System.out.println("hatali cinsiyet girisi");
+            }
+
+        }else { // yas 65'den buyukse
+            System.out.println("emekli olabilirsin");
+        }
+
+
+    }
+}
---
+++ src/ders07_NestedIfElseStatements/C05_NestedIfElseStatements.java
@@ -0,0 +1,45 @@
+package ders07_NestedIfElseStatements;
+
+public class C05_NestedIfElseStatements {
+    public static void main(String[] args) {
+        // Soru 2- Kullanicidan aldigi urun adedi ve ve liste fiyatini alin,
+        // kullaniciya musteri karti olup olmadigini sorun.
+        // Musteri karti varsa 10 urunden fazla alirsa %20, 10 urunden az ise  %15 indirim yapin,
+        // Musteri karti yoksa 10 urunden fazla alirsa %15, 10 urunden az ise  %10 indirim yapin
+
+        int urunAdedi = 25;
+        boolean kartVarMi = false;
+        double listeFiyati = 20;
+        double toplamFiyat = 0;
+
+        // ana degisken kart olsun
+        
+        if (kartVarMi){
+            // kart var
+            if (urunAdedi<0){
+                System.out.println("hatali urun adedi girisi");
+            } else if (urunAdedi<10) {
+                toplamFiyat= urunAdedi*listeFiyati*0.85;
+                System.out.println("%15 indirimli toplam fiyat : " + toplamFiyat + " tl");
+            }else{
+                toplamFiyat= urunAdedi*listeFiyati*0.80;
+                System.out.println("%20 indirimli toplam fiyat : " + toplamFiyat+ " tl");
+            }
+
+
+        } else {
+            // kart yok
+            if (urunAdedi<0){
+                System.out.println("hatali urun adedi girisi");
+            } else if (urunAdedi<10) {
+                toplamFiyat= urunAdedi*listeFiyati*0.90;
+                System.out.println("%10 indirimli toplam fiyat : " + toplamFiyat+ " tl");
+            }else{
+                toplamFiyat= urunAdedi*listeFiyati*0.85;
+                System.out.println("%15 indirimli toplam fiyat : " + toplamFiyat+ " tl");
+            }
+            
+        }
+
+    }
+}
