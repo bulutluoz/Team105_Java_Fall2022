5ca7405d4e3e5353efee3ae1e7cc57d476f6ba7f
ahmet bulutluoz <abbulutluoz@gmail.com>
2022-10-25 21:51:32 +0200

    22.gun sonu

--- src/ders23_constructors/HAraba.java
+++ src/ders23_constructors/HAraba.java
@@ -7,6 +7,37 @@ public class HAraba {
     int yil;
     int fiyat;
 
+   public HAraba(String mrk,String mdl, String ykt , int yl , int fyt){
+
+       marka=mrk;
+       model=mdl;
+       yakit=ykt;
+       yil=yl;
+       fiyat=fyt;
+   }
+
+   public HAraba(String mrk,String mdl,int yl){
+       // istersek parametreleri azaltarak da constructor olusturabiliriz
+       marka=mrk;
+       model=mdl;
+       yil=yl;
+   }
+
+   public HAraba(){
+       // gorunur durumdaki parametresiz constructor
+       // default constructor DEGILDIR
+       // Default constructor'in body'si de bos olur
+       // ancak parametresiz constructor'da body'e kod yazilabilir
+
+       // parametresiz constructor'in body'sinde kod yoksa
+       // default constructor ile ayni islevi gorur
+       // ama yine de gorunur durumdaysa default cons. denmez
+   }
+
+   // biz gozle gorunur herhangi bir constructor olusturdugumuzda
+   // Java default constructor'i siler
+   // Bu durumda daha once olusturulmus olan objelerin sorun olmamasi icin
+   // class'a parametresiz bir constructor eklememiz faydali olur
 
 
     public String toString() {
--- src/ders23_constructors/HArabaRunner.java
+++ src/ders23_constructors/HArabaRunner.java
@@ -2,4 +2,27 @@ package ders23_constructors;
 
 public class HArabaRunner {
 
+    public static void main(String[] args) {
+
+        HAraba arb1= new HAraba();
+        arb1.marka="Toyota";
+        arb1.model="Corolla";
+        arb1.yakit="Dizel";
+        arb1.yil=2010;
+        arb1.fiyat=12000;
+
+        System.out.println(arb1);
+
+
+        HAraba arb2= new HAraba("Honda","Civic","Benzin",2011,11000);
+        System.out.println(arb2);
+
+        HAraba arb3=new HAraba("Opel","Corsa","Dizel",2013,10000);
+        System.out.println(arb3);
+
+
+        HAraba arb4= new HAraba("Tesla","x",2020);
+        System.out.println(arb4);
+
+    }
 }
